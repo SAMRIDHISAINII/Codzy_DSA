a6eecf7046032fc4f2f208b8f504659285b9ef06
Samridhi Saini <83508209+SAMRIDHISAINII@users.noreply.github.com>
2022-07-06 16:51:43 +0530

    leetcode/gfg

---
+++ string/Is Subsequence.cpp
@@ -0,0 +1,22 @@
+// leetcode question number above given 
+// 392. Is Subsequence
+class Solution {
+public:
+
+          bool isSubsequence(string s, string t) {
+       if(t.size()==0 && s.size()==0)
+            return true;
+        if(s.size()>t.size() ||  t.size()==0)
+            return false;
+        int j=0;
+        for(int i=0;i<t.size();i++)
+            if(t[i]==s[j])
+                j++;
+        if(j==s.size())
+            return true;
+        return false;
+
+    }
+
+
+};

35e41771c19c138429bcb011eb3e02a38d3e603d
Samridhi Saini <83508209+SAMRIDHISAINII@users.noreply.github.com>
2022-07-06 20:39:54 +0530

    leetcode/gfg

---
+++ linked list/Linked List Cycle II.cpp
@@ -0,0 +1,19 @@
+//leetcode question number
+// 142 linked List Cycle II
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+           set<ListNode*> visited;
+        ListNode* cur = head;
+        while(cur != NULL){
+            if(visited.find(cur) != visited.end()){
+                return cur;
+            }else{
+                visited.insert(cur);
+            }
+            cur = cur->next;
+        }
+        return NULL;
+        
+    }
+};

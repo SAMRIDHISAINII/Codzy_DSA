39c3008f55829fb2312dafe60b18e03271369d8d
Samridhi Saini <83508209+SAMRIDHISAINII@users.noreply.github.com>
2022-07-20 11:28:21 +0530

    Update Fibonacci Number.cpp

--- DP/Fibonacci Number.cpp
+++ DP/Fibonacci Number.cpp
@@ -1,24 +1,22 @@
 // leetcode question number 509. Fibonacci Number
 class Solution {
 public:
-    int fib(int n) {
-          
-        int *dp = new int[n+1];
+//     dp
+    int fib(int n ){
+     if(n==0) 
+         return 0;
+    if(n==1) return 1;
+//     return fib(n-1)+fib(n-2);
+    
+    int result, prev1=1, prev2=0;
+    for(int i =2; i<=n; i++)
+    {
+        result = prev1+prev2;
+        prev2= prev1;
+        prev1 = result;
         
-        if(n==0)
-            return 0;
-        if(n==1)
-            return 1;
-        
-        dp[0]=0;
-        dp[1] = 1;
-       // dp[2] = 2;
-        
-        for(int i=2; i<=n; i++)
-        {
-            dp[i] = dp[i-1]+dp[i-2];
-        }
-        
-        return dp[n];
+    }
+        return result;
+    
     }
 };

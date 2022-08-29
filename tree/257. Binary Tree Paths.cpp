76c88cedf1d19c460b840b768ecb1162bac7a08c
Samridhi Saini <83508209+SAMRIDHISAINII@users.noreply.github.com>
2022-08-29 06:02:59 +0530

    Create 257. Binary Tree Paths.cpp

---
+++ tree/257. Binary Tree Paths.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void binaryTreePathsUtil(TreeNode* root, vector<string>& vec, string str)
+{
+	if (root)
+	{
+		if (!root->left && !root->right)
+		{
+			str += to_string(root->val);
+			vec.push_back(str);
+		}
+		str = str + to_string(root->val) + "->";
+        
+		binaryTreePathsUtil(root->left, vec, str);
+		binaryTreePathsUtil(root->right, vec, str);
+	}
+}
+
+vector<string> binaryTreePaths(TreeNode* root) {
+	vector<string> vec;
+	string str = "";
+	binaryTreePathsUtil(root, vec, str);
+	return vec;
+}
+};

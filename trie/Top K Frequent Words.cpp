040b0b591d6b4599728e8a37daf81eb542f803fd
Samridhi Saini <83508209+SAMRIDHISAINII@users.noreply.github.com>
2022-09-07 19:15:06 +0530

    Create Top K Frequent Words.cpp

---
+++ trie/Top K Frequent Words.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+          unordered_map<string, int> counter;
+        
+        auto comp = [&counter](const string& lhs, const string& rhs){
+         
+            return (counter[lhs] == counter[rhs]) ? (lhs > rhs) : 
+                (counter[lhs] < counter[rhs]);
+        };
+        
+        priority_queue<string , vector<string>, decltype(comp)> pq(comp);
+        vector<string> ans;
+        
+        for(string& word : words){
+            counter[word]++;
+        }
+        
+     
+        for(auto it = counter.begin(); it != counter.end(); it++){
+            pq.push(it->first);
+        }
+        
+        while(k-- > 0){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        
+        return ans;
+    }
+};

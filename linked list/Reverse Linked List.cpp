e0ee046f8e6117a2eaa836e9f0683ff8aae4a3f0
Samridhi <83508209+SAMRIDHISAINII@users.noreply.github.com>
2023-01-16 07:18:13 +0530

    Update Reverse Linked List.cpp

--- linked list/Reverse Linked List.cpp
+++ linked list/Reverse Linked List.cpp
@@ -16,3 +16,26 @@ public:
         
     }
 };
+
+
+
+//
+
+
+
+ListNode* reversList(ListNode* head) {
+  if(!head || !head->next) 
+    // checking if head is null or nextpointer of head is null
+    return head;
+  
+}
+ ListNode* reversed = reverseList(head->next); 
+//recursively call the function with next pointer of head 
+
+head->next->next = head;
+// update the next pointer of previouse node to point to the current node
+
+head->next = NULL;
+// update the current ndoe's next pointer to null
+return reversed;
+}

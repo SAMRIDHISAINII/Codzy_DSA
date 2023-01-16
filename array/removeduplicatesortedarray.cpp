056b22e996fd15a58201cccac2cc7feefe8c6bed
Samridhi <83508209+SAMRIDHISAINII@users.noreply.github.com>
2023-01-16 07:20:09 +0530

    Update removeduplicatesortedarray.cpp

--- array/removeduplicatesortedarray.cpp
+++ array/removeduplicatesortedarray.cpp
@@ -50,3 +50,40 @@ public:
 //     }
 // }
 // // } Driver Code Ends
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    // Initialize two pointers, i and j
+    int i = 0, j = 1;
+    // Iterate through the vector starting from the second element
+    while (j < nums.size()) {
+        // If the current element at i is not equal to the current element at j
+        if (nums[i] != nums[j]) {
+            // Increment i and copy the element at j to the next position
+            i++;
+            nums[i] = nums[j];
+        }
+        // Increment j to move to the next element
+        j++;
+    }
+    // Return the new size of the vector after duplicates have been removed
+    return i + 1;
+}
+
+int main() {
+    // Initialize a vector
+    vector<int> nums = {1, 2, 2, 3, 3, 4, 5, 5, 6};
+    // Call the removeDuplicates function
+    int newSize = removeDuplicates(nums);
+    cout << "New size of vector after removing duplicates: " << newSize << endl;
+    // Print the vector after removing duplicates
+    cout << "Vector after removing duplicates: ";
+    for (int i = 0; i < newSize; i++) {
+        cout << nums[i] << " ";
+    }
+    return 0;
+}
